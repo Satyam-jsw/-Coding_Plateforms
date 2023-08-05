89e6d3cde9fbc61287da38275c5ef16783a95234
Gurjarrahul <rahulgur08@gmail.com>
2023-08-05 17:53:48 +0530

    Initial commit

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -1,80 +1,80 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
-
+const url='https://coding-platform-bitcode.onrender.com'
 module.exports = function (app) {
   app.use(
     '/sendq',
     createProxyMiddleware({
-      target: 'http://127.0.0.1:8000',
+      target: url,
       changeOrigin: true,
     })
   );
   app.use(
     '/test',
     createProxyMiddleware({
-      target: 'http://127.0.0.1:8000',
+      target: url,
       changeOrigin: true,
     })
   );
   app.use(
     '/submit',
     createProxyMiddleware({
-      target: 'http://127.0.0.1:8000',
+      target: url,
       changeOrigin: true,
     })
   );
   app.use(
     '/qlist',
     createProxyMiddleware({
-      target: 'http://127.0.0.1:8000',
+      target: url,
       changeOrigin: true,
     })
   );
   app.use(
     '/add',
     createProxyMiddleware({
-      target: 'http://127.0.0.1:8000',
+      target: url,
       changeOrigin: true,
     })
   );
   app.use(
     '/input',
     createProxyMiddleware({
-      target: 'http://127.0.0.1:8000',
+      target: url,
       changeOrigin: true,
     })
   );
   app.use(
     '/register',
     createProxyMiddleware({
-      target: 'http://127.0.0.1:8000',
+      target: url,
       changeOrigin: true,
     })
   )
   app.use(
     '/login',
     createProxyMiddleware({
-      target: 'http://127.0.0.1:8000',
+      target: url,
       changeOrigin: true,
     })
   )
   app.use(
     '/sendmail',
     createProxyMiddleware({
-      target: 'http://127.0.0.1:8000',
+      target: url,
       changeOrigin: true,
     })
   )
   app.use(
     '/forgetpassword/:token',
     createProxyMiddleware({
-      target: 'http://127.0.0.1:8000',
+      target: url,
       changeOrigin: true,
     })
   )
   app.use(
     '/home',
     createProxyMiddleware({
-      target: 'http://127.0.0.1:8000',
+      target: url,
       changeOrigin: true,
     })
   )
